15b77fd412d0d7ed9b3c8889c4b0c83eb1e97463
aracarabeo <cbcarabeo@up.edu.ph>
2017-05-03 13:35:30 +0800

    Update

--- gamelogic.h
+++ gamelogic.h
@@ -6,18 +6,57 @@
 #define KEY_ESC 27
 
 
-int check(int board[]){
-	if(board[0] != 0 && board[0] == board[1] && board[0] == board[2]) return board[0];
-	else if(board[3] != 0 && board[3] == board[4] && board[3] == board[5]) return board[3];
-	else if(board[6] != 0 && board[6] == board[7] && board[6] == board[8]) return board[6];
-	else if(board[0] != 0 && board[0] == board[3] && board[0] == board[6]) return board[0];
-	else if(board[1] != 0 && board[1] == board[4] && board[1] == board[7]) return board[1];
-	else if(board[2] != 0 && board[2] == board[5] && board[2] == board[8]) return board[2];
-	else if(board[0] != 0 && board[0] == board[4] && board[0] == board[8]) return board[0];
-	else if(board[6] != 0 && board[6] == board[4] && board[6] == board[2]) return board[6];
+int check(int board[],int computer){
+	if(board[0] != 0 && board[0] == board[1] && board[0] == board[2]){
+		if(board[0] == computer){
+			return 1;
+		}
+		return -1;
+	}
+	else if(board[3] != 0 && board[3] == board[4] && board[3] == board[5]){
+		if(board[3] == computer){
+			return 1;
+		}
+		return -1;
+	}
+	else if(board[6] != 0 && board[6] == board[7] && board[6] == board[8]){
+		if(board[6] == computer){
+			return 1;
+		}
+		return -1;
+	}
+	else if(board[0] != 0 && board[0] == board[3] && board[0] == board[6]){
+		if(board[0] == computer){
+			return 1;
+		}
+		return -1;
+	}
+	else if(board[1] != 0 && board[1] == board[4] && board[1] == board[7]){
+		if(board[1] == computer){
+			return 1;
+		}
+		return -1;
+	}
+	else if(board[2] != 0 && board[2] == board[5] && board[2] == board[8]){
+		if(board[2] == computer){
+			return 1;
+		}
+		return -1;
+	}
+	else if(board[0] != 0 && board[0] == board[4] && board[0] == board[8]){
+		if(board[0] == computer){
+			return 1;
+		}
+		return -1;
+	}
+	else if(board[6] != 0 && board[6] == board[4] && board[6] == board[2]){
+		if(board[6] == computer){
+			return 1;
+		}
+		return -1;
+	}
 	else return 0;	
 }
-
 void play(int board[],int move){
 	int cur_x = 4, cur_y = 21, x = 0, y = 0, choice = 0, base_y;
 	int input = 0, index;
@@ -79,6 +118,16 @@ void play(int board[],int move){
 	}
 }
 
+int terminal(int board[9]){
+	int i;
+	for(i=0;i<9;i++){
+		if(board[i] == 0){
+			return -1;
+		}
+	}
+	return 1;
+}
+
 void startGame(){
 	int board[9] = {0,0,0,0,0,0,0,0,0};
 	int playerFlag = rand()%2;
@@ -86,29 +135,34 @@ void startGame(){
 	int player_turn = 0;
 	int turn = 0;
 	int temp;
-
+	int computer = -1;
+	int player = 1;
 	if(playerFlag == 0){
-		player = 1;
-		computer = -1;
 		player_turn = 0;
 	}
 	else if(playerFlag == 1){
-		computer = 1;
-		player = -1;
 		player_turn = 1;
 
 	}
-	for(turn=0;turn<9 && check(board)== 0;turn++){
+	for(turn=0;turn<9 && terminal(board)==-1 && check(board,computer)==0;turn++){
 		if((turn+player_turn)%2 != 0){
 			play(board,player);
 		}
 		else{
-			computerplay(board,computer);
+			findBest(board,computer);
 		}
 	}
-
 	clrscr();
-	printf("\n____ WINS!\n");
+	int score = check(board,computer);
+	if(check(board,computer)==1){
+		printf("Computer Won!\n");
+	}
+	else if(check(board,computer)==-1){
+		printf("Player Won!\n");
+	}
+	else{
+		printf("Draw\n");
+	}
 	drawBoard(board);
 	printf("\nPress any button to continue...\n");
 	getch();
